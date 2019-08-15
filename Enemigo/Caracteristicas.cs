efd1eb13d47508c9029c2e3352135b3e6abcdf93
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-15 07:44:57 -0700

    Agregar archivos de proyecto.

---
+++ Enemigo/Caracteristicas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enemigo
+{
+    class Caracteristicas
+    {
+        public int posisionx { get; set; }
+        public int posiciony { get; set; }
+        private int _puntosG;
+        public int puntosG
+
+        {
+            get
+            {
+                return _puntosG;
+            }
+            set
+            {
+                if (value == 100)
+                {
+                    _puntosG = value;
+                }
+                else
+                {
+                    throw new Exception("No consiguio los suficientes");
+                }
+
+            }
+
+
+        }
+
+        public void moverse() { }
+        public void  disparar() { }
+        public void explotar() { }
+        
+
+    }
+}
